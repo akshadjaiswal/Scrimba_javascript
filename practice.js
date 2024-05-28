070f2ae230798775685223972f34f898ad7546d1
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-05-28 23:58:12 +0530

    Update Practice.js

--- practice.js
+++ practice.js
@@ -10,8 +10,27 @@ let name = "Akshad";
 let greeting = "Hi there";
 
 // Create a function that logs out "Hi there, Linda!" when called
-
 const greet = () => {
   console.log(greeting + "," + " " + name + "!");
 };
 greet();
+
+let myPoints = 3;
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+
+function add3Points() {
+  myPoints = myPoints + 3;
+}
+function remove1Point() {
+  myPoints = myPoints - 1;
+}
+
+// Call the functions to that the line below logs out 10
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+console.log(myPoints);
