72312c48025c3e3e3bc02a6abd90ee99cd8c3d83
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-05-28 23:04:34 +0530

    practice

--- practice.js
+++ practice.js
@@ -1,1 +1,17 @@
-const
+// Create two variables, firstName and lastName
+let firstName = "Akshad";
+let lastName = " Jaiswal";
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + lastName;
+// Log fullName to the console
+console.log(fullName);
+
+let name = "Akshad";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+const greet = () => {
+  console.log(greeting + "," + " " + name + "!");
+};
+greet();
