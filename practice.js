81c40c3d79bea33bed3ea541dea043fd21be25ee
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-23 23:43:22 +0530

    push() pop() shift() unshift()

--- practice.js
+++ practice.js
@@ -100,3 +100,17 @@ The 5 largest countries in the world:
 for (let i = 0; i < largeCountries.length; i++) {
   console.log(largeCountries[i]);
 }
+
+let largeCountries1 = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+largeCountries1.pop()
+largeCountries1.push("Pakistan")
+largeCountries1.shift()
+largeCountries1.unshift("China")
+console.log(largeCountries1)
