b77822242254435c580f1aa81f95027b159fe04f
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-23 23:30:56 +0530

    Objects and functions

--- practice.js
+++ practice.js
@@ -34,3 +34,30 @@ add3Points();
 remove1Point();
 remove1Point();
 console.log(myPoints);
+
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+
+// Create a function, logData(), that uses the person object to create a
+// string in the following format:
+// "Per is 35 years old and lives in Norway"
+
+// Call the logData() function to verify that it works
+
+let person = {
+  name: "Akshad",
+  age: 22,
+  country: "Pune",
+};
+
+function logData() {
+  console.log(
+    person.name +
+      " is " +
+      person.age +
+      " years old and lives in " +
+      person.country
+  );
+}
+
+logData();
